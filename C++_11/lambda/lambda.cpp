43ca17cf71eb37a3907e0e0b821d0ec6e8bface2
GopC <gopi.c@harman.com>
2025-05-20 16:26:15 +0530

    C++11 lambda program

---
+++ C++_11/lambda/lambda.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <functional>
+using namespace std;
+int main()
+{
+	int x=2;
+	int y=3;
+	
+	//std::function<int(int,int)> sum = [=] (int a,int b) -> int {  // [=] it will alow all global values into the lambda function for readonly i.e not modifiable. 
+	std::function<int(int,int)> sum = [&] (int a,int b) -> int { // [&] it will alow all global values into the lambda function for read and write i.e modifiable.
+	//std::function<int(int,int)> sum = [&x, &y] (int a,int b) -> int { // [&x, &y] it will alow x,y values into the lambda function for read and write i.e modifiable.
+	//std::function<int(int,int)> sum = [x,y] (int a,int b) -> int { // [x,y] it will alow x, y values into the lambda function for readonly i.e not modifiable.
+		x = 10;
+		y = 20;
+		return x + y;
+	};
+	cout<<"sum = "<<sum(x,y)<<endl;
+	return 0;
+}
