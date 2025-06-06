74686e912fc887b497d2b9ecba47e02c2459d09a
Chappidi Gopi <chappidi.gopi@capgemini.com>
2025-06-06 13:37:55 +0000

    understanding unary,binary,transformation traits programs

---
+++ C++_11/traits/transformation_traits.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <type_traits>
+using namespace std;
+/*
+ * add_const
+ * add_volatile
+ * make_unsigned
+ */
+
+int main()
+{
+	// DO Impliment..!
+	cout<<"DO Impliment Transformation Traits..!"<<endl;
+	return 0;
+}
