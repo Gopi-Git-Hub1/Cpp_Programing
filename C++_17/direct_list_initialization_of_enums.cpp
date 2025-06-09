198957f913c3cab2b7ab98d0826d882603cf7c50
Chappidi Gopi <chappidi.gopi@outlook.com>
2025-06-09 06:32:43 +0000

    C++_17 Features programs

---
+++ C++_17/direct_list_initialization_of_enums.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"Do Impliment Direct list initialization of enums..!"<<endl;
+	return 0;
+}
---
+++ C++_17/fold_expressions.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"Do Impliment Fold Expressions..!"<<endl;
+	return 0;
+}
