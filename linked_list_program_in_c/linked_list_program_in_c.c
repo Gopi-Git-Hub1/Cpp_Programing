b186a01b2c012eb79b532ff05b1d96f1c52a0160
Chappidi Gopi <chappidi.gopi@outlook.com>
2025-06-09 13:32:44 +0000

    Code to Insert a Node at a Specific Position in a Linked List in C

--- linked_list_program_in_c/linked_list_program_in_c.c
+++ linked_list_program_in_c/linked_list_program_in_c.c
@@ -22,7 +22,7 @@ void setDataIntoFront()
 		head = frontNode;	
 	}
 }
-void setDataIntoStack(){
+void setDataIntoEnd(){
 	struct node *newNode = NULL;
 		newNode = (var*)malloc(sizeof(var));
 		printf("Enter the Data : ");
@@ -44,6 +44,41 @@ void setDataIntoStack(){
 	}
 
 }
+void setDataSpecificPosition()
+{
+	int pos;
+	int nNodes = 0;
+	var *nofNodes = head;
+	while(nofNodes != NULL){
+		nofNodes = nofNodes->next;
+		nNodes++;
+	}
+	printf("position : ");
+	scanf("%d",&pos);
+	if(pos < 1 )
+		printf("invalid position..!\n");
+	else if(pos == 1)
+		setDataIntoFront();
+	else if(pos > nNodes)
+	{
+		 printf("position is greater than number of nodes..!\n");
+	}
+	else if(pos <= nNodes)
+	{
+		var *tmp  = head;
+		var *newNode = (var*)malloc(sizeof(var));
+		printf("Enter the Data : ");
+		scanf("%d",&newNode->data);
+		newNode->next = NULL;
+			for(int i =1;i<pos-1;i++)
+			{
+				tmp=tmp->next;
+			}
+			 newNode->next = tmp->next;
+			 tmp->next = newNode;
+	}
+
+}
 void popDataFromFront()
 {
 	var *pop=head;
@@ -55,7 +90,7 @@ void popDataFromFront()
 	free(pop);
 	printf("SUCCESSFULLY DELETED FIRST NODE FROM LIST\n");
 }
-void popDataFromStack()
+void popDataFromEnd()
 {
 	if (head == NULL) {
 		printf("The list is empty.\n");
@@ -78,19 +113,26 @@ void popDataFromStack()
 	printf("Node from the end of the list deleted successfully!!\n");
 
 }
-void displayStack()
+void popDataSpecificPosition()
+{
+
+}
+void displayData()
 {
 	var *tmp = head;
 	if (head == NULL) {
 		printf("The list is empty.\n");
 	}
+	int n = 0;
 	printf("Stack Data\n");
 	while(tmp != NULL)
 	{
 		printf("%d ",tmp->data);
 		tmp = tmp->next;
+		n++;
 	}
 	printf("\n");
+	printf("no of nodes : %d\n",n);
 }
 int main()
 {
@@ -98,27 +140,33 @@ int main()
 	while(1)
 	{
 	printf("Choose Options:\n");
-	printf("1) setDataIntoStack\n");
-	printf("2) displayStack\n");
-	printf("3) popDataFromStack\n");
+	printf("1) setDataIntoEnd\n");
+	printf("2) displayData\n");
+	printf("3) popDataFromEnd\n");
 	printf("4) exit\n");
 	printf("5) setDataIntoFront\n");
 	printf("6) popDataFromFront\n");
+	printf("7) setDataSpecificPosition\n");
+	printf("8) popDataSpecificPosition\n");
 	scanf("%d",&option);
 	switch(option)
 	{
-		case 1: setDataIntoStack();
+		case 1: setDataIntoEnd();
 
 			break;
-		case 2: displayStack();
+		case 2: displayData();
 			break;
-		case 3: popDataFromStack();
+		case 3: popDataFromEnd();
 			break;
 		case 4:
 			exit(0);
 		case 5: setDataIntoFront();
 			break;
 		case 6: popDataFromFront();
+			break;
+		case 7: setDataSpecificPosition();
+			break;
+		case 8: popDataSpecificPosition();
 			break;
 		default :
 			printf("Choose correct option...!\n");
