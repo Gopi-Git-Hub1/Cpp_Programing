73fae682de363b400125ef81e7e27bd28ac0ceab
Chappidi Gopi <chappidi.gopi@capgemini.com>
2025-06-09 05:23:21 +0000

    current date,time,line and file

--- C++_11/traits/transformation_traits.cpp
+++ C++_11/traits/transformation_traits.cpp
@@ -11,5 +11,9 @@ int main()
 {
 	// DO Impliment..!
 	cout<<"DO Impliment Transformation Traits..!"<<endl;
+	cout<<"Current date : "<<__DATE__<<endl;
+	cout<<"Current time : "<<__TIME__<<endl;
+	cout<<"Current line : "<<__LINE__<<endl;
+	cout<<"Current file : "<<__FILE__<<endl;
 	return 0;
 }
