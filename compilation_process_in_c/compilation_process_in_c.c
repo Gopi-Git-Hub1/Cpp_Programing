83be20f4bd10e4a66d904f1235ed482bfcc8309c
Chappidi Gopi <chappidi.gopi@capgemini.com>
2025-06-11 10:47:43 +0000

    compilation process program

---
+++ compilation_process_in_c/compilation_process_in_c.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define PI 3.14159265
+int main(){
+   
+   /* My Compilation Process program in C */
+   
+	float areaCircle;
+	int radios = 5;
+	areaCircle = PI*radios*radios;
+   printf("Area of Circle : %f \n",areaCircle);
+
+   return 0;
+}
