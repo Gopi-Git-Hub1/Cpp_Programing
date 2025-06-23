611b1386b1785bd1d8f8d41be5bf6e2a736fd56e
Chappidi Gopi <chappidi.gopi@capgemini.com>
2025-06-23 10:04:54 +0000

    socket programming in cpp

---
+++ socket_programming_in_cpp/client.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstring>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+using namespace std;
+int main()
+{
+	// Creating the Client Socket
+	int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+	// Defining Server Address
+	sockaddr_in serverAddress;
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_port = htons(8080);
+	serverAddress.sin_addr.s_addr = INADDR_ANY;
+	// Connecting to the Server
+	connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+	// Sending Data to the Server
+	const char* message = "Hello, server!"; 
+	send(clientSocket, message, strlen(message), 0);
+	// Closing the Client Socket
+	close(clientSocket);
+	return 0;
+}
